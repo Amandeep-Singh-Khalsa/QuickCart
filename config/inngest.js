8e0642d8f5f3fb199018bcac897543f6f72a7286
Amandeep-Singh-Khalsa <amandeepsk1512@gmail.com>
2025-07-02 22:22:53 +0530

    Finished

--- config/inngest.js
+++ config/inngest.js
@@ -5,7 +5,7 @@ import Order from "@/models/Order.js";
 
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "quickcart-next" });
+export const inngest = new Inngest({ id: "quickcart-next",apiBase: "http://localhost:8288" });
 
 
 // Inngest Function to save user data to database
